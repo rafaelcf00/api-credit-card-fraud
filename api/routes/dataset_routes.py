1595bf2acbd945893bc359d7cbd9e9956c680f06
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2024-04-29 19:44:56 -0300

    feat: route csv to json

---
+++ api/routes/dataset_routes.py
@@ -0,0 +1,20 @@
+import os
+from fastapi import APIRouter, HTTPException
+from api.common.convert_csv_json import convert_to_json
+
+router = APIRouter(tags=['Dataset'])
+
+directory = 'dataset/'
+filename = 'fraud_dataset.csv'
+
+@router.get('/{filename}', status_code=200)
+async def export_json():
+    csv_path = os.path.join(directory, filename)
+    try:
+        if csv_path:
+            json_dataset = convert_to_json(csv_path)
+            return json_dataset
+        else:
+            raise HTTPException(status_code=404, detail='File not found.')
+    except Exception as error:
+        HTTPException(status_code=500, detail=str(error))
