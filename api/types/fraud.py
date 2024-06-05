4120cb1c065336d3014900215c3f752bddcbf055
rafaelcf00 <rafacesar0070@gmail.com>
2024-06-05 19:54:13 -0300

    fixessssssssss att

--- api/types/fraud.py
+++ api/types/fraud.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
 
+
 class ResponseDatasetType(BaseModel):
-    id: Optional[int] | None = None
+    id: Optional[int] = None
     is_fraud: int
     input_id: int
